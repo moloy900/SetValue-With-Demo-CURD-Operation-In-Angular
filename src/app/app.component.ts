0485d84b4498e54b268c8203045155eaee2c6a88
bleads2 <bleads2@gmail.com>
2023-09-05 06:14:24 -0700

    setValue() with DEMO--CURD-OPERATION

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
+// Angular Component
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angularprac1';
+
+  addVal: any[] = [];
+  editMode: boolean = false;
+  index: any;
+
+  newForm = new FormControl();
+
+  getAdd() {
+    const newData = this.newForm.value;
+    if (this.editMode) {
+      this.addVal[this.index].name = newData;
+      this.editMode = false;
+    } else {
+      this.addVal.push({
+        name: newData,
+      });
+    }
+    this.newForm.reset();
+  }
+
+  removeData() {
+    this.addVal.pop(); // Remove the last item from the array
+  }
+
+  onDeleteItems(c: any) {
+    this.addVal.splice(c, 1);
+  }
+
+  onEditItems(d: any) {
+    this.editMode = true;
+    this.index = d;
+    this.newForm.setValue(this.addVal[d].name);
+  }
 }
